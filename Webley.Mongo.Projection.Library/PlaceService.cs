badcf4ce53ef90a426bac7269e5fe4173ec0287b
Dave Wolsey <david.wolsey@gmail.com>
2023-07-21 13:48:14 +0100

    Tidied whitespace

--- Webley.Mongo.Projection.Library/PlaceService.cs
+++ Webley.Mongo.Projection.Library/PlaceService.cs
@@ -7,26 +7,26 @@ public class PlaceService
 {
     private readonly IMongoCollection<PlaceEntity> _places;
 
-	public PlaceService(IMongoCollection<PlaceEntity> places)
-	{
-		_places = places;
-	}
+    public PlaceService(IMongoCollection<PlaceEntity> places)
+    {
+        _places = places;
+    }
 
-	public async Task<List<PlaceEntity>> GetPlaceEntities()
-	{
-		return await _places.Find(Builders<PlaceEntity>.Filter.Empty).ToListAsync();
-	}
+    public async Task<List<PlaceEntity>> GetPlaceEntities()
+    {
+        return await _places.Find(Builders<PlaceEntity>.Filter.Empty).ToListAsync();
+    }
 
     public async Task<List<PlaceHeader>> GetPlaceHeaders()
     {
         return await _places
-			.Find(Builders<PlaceEntity>.Filter.Empty)
-			.Project(x => new PlaceHeader
-			{
-				Name = x.Name,
-				Latitude = x.Location != null ? x.Location.X : null,
+            .Find(Builders<PlaceEntity>.Filter.Empty)
+            .Project(x => new PlaceHeader
+            {
+                Name = x.Name,
+                Latitude = x.Location != null ? x.Location.X : null,
                 Longitude = x.Location != null ? x.Location.Y : null,
             })
-			.ToListAsync();
+            .ToListAsync();
     }
 }
